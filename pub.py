4a70b7de3bc5f78312e1a67b90bec491587c3f20
sandrine <sandrine.marquis30@gmail.com>
2016-05-10 14:07:03 -0400

    test mqtt

---
+++ pub.py
@@ -0,0 +1,17 @@
+from random import randint
+import time
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect('localhost', 1883, 60)
+
+while True:
+    value1 = randint(0,9)
+    print("1 :", value1)
+    client.publish('/leds/esp8266_1', value1)
+
+    value2 = randint(0,9)
+    print("2 :", value2)
+    client.publish('/leds/esp8266_2', value2)
+
+    time.sleep(1)

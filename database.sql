4e629550d4d8c4adf53067cf02a70f49aacad73c
sandrine <sandrine.marquis30@gmail.com>
2016-05-11 16:16:40 -0400

    test mariadb

--- database.sql
+++ database.sql
@@ -1,10 +1,8 @@
 CREATE TABLE domocontrolsm.tempsensors (
 	id int(10) NOT NULL auto_increment,
 	date varchar(25) NOT NULL,
-	topic varchar(60),
-	temp decimal(5,2),
-	humidity decimal(5,2),
-	PRIMARY KEY (date)
+	topic varchar(60) NOT NULL,
+	temp decimal(5,2) NOT NULL,
+	humidity decimal(5,2) NOT NULL,
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE UNIQUE INDEX id ON domocontrolsm.tempsensors (id);
--- sub.py
+++ sub.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
+import mysql.connector as mariadb
 import datetime
 import time
+from decimal import Decimal
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -8,17 +10,32 @@ def on_connect(client, userdata, rc):
     print(
         "Connected with result code " + str(rc))  # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe([("/home/room/esp8266_1", 0), ("/home/room/esp8266_2", 2), ("/home/room/esp8266_3", 2), ("/home/room/esp8266_4", 2),
-                      ("/home/room/esp8266_5", 2)])  # The callback for when a PUBLISH message is received from the server.
+    client.subscribe([("/home/room/esp8266_1", 0), ("/home/room/esp8266_2", 2), ("/home/room/esp8266_3", 2),
+                      ("/home/room/esp8266_4", 2), ("/home/room/esp8266_5",
+                                                    2)])  # The callback for when a PUBLISH message is received from the server.
 
 
 def on_message(client, userdata, msg):
     f = open('myfile.csv', 'a')
-    print(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "," + msg.topic + "," + str(
+    datenow = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    print(datenow + "," + msg.topic + "," + str(
         msg.payload.decode("UTF-8")))
-    f.write(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') + "," + msg.topic + "," + str(
+    f.write(datenow + "," + msg.topic + "," + str(
         msg.payload.decode("UTF-8")) + "\n")
     f.close()
+
+    datas = msg.payload.decode("UTF-8").split(",")
+    topic = msg.topic
+    value1 = datas[0]
+    value2 = datas[1]
+
+    mariadb_connection = mariadb.connect(user='domocontrolsm_user', password='domocontrolsm_password',
+                                         database='domocontrolsm_database')
+    cursor = mariadb_connection.cursor()
+    cursor.execute("INSERT INTO tempsensors(date,topic,temp,humidity) VALUES(%s,%s,%s,%s)",
+                   (datenow, topic, Decimal(value1), Decimal(value2)))
+    mariadb_connection.commit()
+    mariadb_connection.close()
 
 
 client = mqtt.Client()
